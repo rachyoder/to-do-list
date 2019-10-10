fe513111e51acaa2eda1d9b2382c4e45526af34f
Rachael Yoder <wayoder4@gmail.com>
2019-10-10 16:06:47 -0400

    working updating list

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
-LIST_ITEMS = {};
-ITEMS_ARRAY = [];
-ITEM_ID = 0;
-
-ITEMS_ARRAY = JSON.parse(localStorage.getItem("todos", LIST_ITEMS));
+let LIST_ITEMS = {};
+let ITEMS_ARRAY = [];
+let ITEM_ID = 0;
+function fetchLocalData() {
+    let temp = localStorage.getItem("todos");
+    if (temp != null) {
+        ITEMS_ARRAY = JSON.parse(temp);
+    }
+    populateTable();
+}
 
 /* Input Constructor */
 
 class Input {
-    constructor(name, id, completed, archive) {
+    constructor(name, id) {
         this.name = name;
         this.id = id;
         this.completed = false;
-        this.archive = false;
+        this.delete = false;
     }
 }
 // let item = JSON.parse(localStorage.getItem("todos", LIST_ITEMS));
@@ -19,7 +24,7 @@ class Input {
 
 function storeInput(e) {
     if (e.keyCode === 13) {
-        let latestItem = document.getElementById("input_text").value;
+        let latestItem = e.target.value;
         document.getElementById("input_text").value = "";
         console.log({ latestItem });
 
@@ -31,9 +36,47 @@ function storeInput(e) {
         ITEMS_ARRAY.push(input);
         LIST_ITEMS = ITEMS_ARRAY;
         localStorage.setItem("todos", JSON.stringify(LIST_ITEMS));
+        populateTable();
     }
 }
 
 function populateTable() {
+    let table_main = document.createElement("table");
+    let table_body = document.createElement("tbody");
 
+    table_main.setAttribute("class", " table table-borderless mx-5")
+
+
+    for (let j = 0; j < ITEMS_ARRAY.length; j++) {
+        let table_row = document.createElement("tr");
+        for (let i = 0; i < 3; i++) {
+            if (i == 0) {
+                let table_item = document.createElement("td");
+                let complete_checkbox = document.createElement("input")
+                complete_checkbox.setAttribute("type", "checkbox");
+                table_item.appendChild(complete_checkbox);
+                table_row.appendChild(table_item);
+            } else if (i == 1) {
+                let table_item = document.createElement("td");
+                table_item.setAttribute("class", "text-center");
+                table_item.setAttribute("style", "word-break: break-all;")
+                console.log({ ITEMS_ARRAY, ITEM_ID })
+                console.log({ ITEMS_ARRAY, })
+                table_item.innerHTML = ITEMS_ARRAY[j]["name"];
+                table_row.appendChild(table_item);
+            } else if (i == 2) {
+                let table_item = document.createElement("td");
+                let delete_button = document.createElement("button");
+                // delete_button.setAttribute("class","close");
+
+                table_item.appendChild(delete_button);
+                table_row.appendChild(table_item);
+            }
+        }
+        table_body.appendChild(table_row);
+    }
+    table_main.appendChild(table_body);
+    let row = document.getElementById("list_row");
+    row.innerHTML = "";
+    row.appendChild(table_main);
 }
--- js/page.js
+++ js/page.js
@@ -1,29 +1,42 @@
 /* Page Layout */
 let APP = document.getElementById("app");
 APP.setAttribute("class", "container");
+function init() {
+    for (let i = 0; i < 4; i++) {
+        if (i == 0) {
+            let title_row = document.createElement("div");
+            let title_col = document.createElement("div");
+            title_row.setAttribute("class", "row");
+            title_col.setAttribute("class", "col-12 display-4 text-center");
+            title_col.innerHTML = "To-<span class='text-muted'>Dos</span>"
+            title_row.appendChild(title_col);
+            APP.appendChild(title_row);
+        } else if (i == 1) {
+            let input_row = document.createElement("div");
+            let input_col = document.createElement("div");
+            let input_text = document.createElement("input");
 
-for (let i = 0; i < 4; i++) {
-    if (i == 0) {
-        let title_row = document.createElement("div");
-        let title_col = document.createElement("div");
-        title_row.setAttribute("class", "row");
-        title_col.setAttribute("class", "col-12 display-4 text-center");
-        title_col.innerHTML = "To-<span class='text-muted'>Dos</span>"
-        title_row.appendChild(title_col);
-        APP.appendChild(title_row);
-    } else if (i == 1) {
-        let input_row = document.createElement("div");
-        let input_col = document.createElement("div");
-        let input_text = document.createElement("input");
+            let list_row = document.createElement("div");
+            list_row.setAttribute("id", "list_row");
 
-        input_row.setAttribute("class","row");
-        input_col.setAttribute("class","col-12 mx-auto");
-        input_text.setAttribute("class", "w-100");
-        input_text.setAttribute("type", "text");
-        input_text.setAttribute("id", "input_text");
-        input_text.addEventListener("keydown", storeInput);
-        input_col.appendChild(input_text);
-        input_row.appendChild(input_col);
-        APP.appendChild(input_row);
+            input_row.setAttribute("class", "row");
+            input_col.setAttribute("class", "col-12 mx-auto");
+            input_text.setAttribute("class", "w-100");
+            input_text.setAttribute("style", "border: solid black 1px; border-radius: 5px;")
+            input_text.setAttribute("type", "text");
+            input_text.setAttribute("id", "input_text");
+
+            input_text.addEventListener("keydown", storeInput);
+
+            list_row.setAttribute("class", "row");
+
+            input_col.appendChild(input_text);
+            input_row.appendChild(input_col);
+            // list_row.appendChild(table);
+            APP.appendChild(input_row);
+            APP.appendChild(list_row);
+        } 
+        
     }
+    fetchLocalData();
 }
