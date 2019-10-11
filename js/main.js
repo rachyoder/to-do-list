caa47d4c9bbb3c6d3295d6306794ecf98b4e562e
Rachael Yoder <wayoder4@gmail.com>
2019-10-10 20:24:15 -0400

    completed and deleted buttons working

--- js/main.js
+++ js/main.js
@@ -25,8 +25,6 @@ function storeInput(e) {
     if (e.keyCode === 13) {
         let latestItem = e.target.value;
         document.getElementById("input_text").value = "";
-        console.log({ latestItem });
-
         let input = new Input(
             latestItem,
             ITEM_ID
@@ -47,35 +45,48 @@ function populateTable() {
 
 
     for (let j = 0; j < ITEMS_ARRAY.length; j++) {
-        let table_row = document.createElement("tr");
-        for (let i = 0; i < 3; i++) {
-            if (i == 0) {
-                let table_item = document.createElement("td");
-                let complete_checkbox = document.createElement("input")
-                complete_checkbox.setAttribute("type", "checkbox");
-                complete_checkbox.setAttribute("id", j);
-                complete_checkbox.addEventListener("change", checkCompleted);
-                table_item.appendChild(complete_checkbox);
-                table_row.appendChild(table_item);
-            } else if (i == 1) {
-                let table_item = document.createElement("td");
-                table_item.setAttribute("class", "text-center");
-                table_item.setAttribute("style", "word-break: break-all;")
-                console.log({ ITEMS_ARRAY, ITEM_ID })
-                console.log({ ITEMS_ARRAY, })
-                table_item.innerHTML = ITEMS_ARRAY[j]["name"];
-                table_row.appendChild(table_item);
-            } else if (i == 2) {
-                let table_item = document.createElement("td");
-                let delete_button = document.createElement("button");
-                delete_button.setAttribute("class", "btn btn-light text-secondary px-1 py-0");
-                delete_button.innerHTML = "X";
+        if (ITEMS_ARRAY[j].delete == false) {
+            let table_row = document.createElement("tr");
+            for (let i = 0; i < 3; i++) {
+                if (i == 0) {
+                    let table_item = document.createElement("td");
+                    let complete_checkbox = document.createElement("input")
+                    complete_checkbox.setAttribute("type", "checkbox");
+                    complete_checkbox.setAttribute("id", j);
+                    if (ITEMS_ARRAY[j].completed == true) {
+                        complete_checkbox.setAttribute("checked", "true");
+                    } else {
+                        complete_checkbox.setAttribute("unchecked", "true");
+                    }
+                    complete_checkbox.addEventListener("change", checkCompleted);
+                    table_item.appendChild(complete_checkbox);
+                    table_row.appendChild(table_item);
+                } else if (i == 1) {
+                    let table_item = document.createElement("td");
+                    table_item.setAttribute("id", "name" + j);
+                    table_item.setAttribute("class", "text-center");
+                    table_item.setAttribute("style", "word-break: break-word;");
+                    if (ITEMS_ARRAY[j].completed == true) {
+                        table_item.setAttribute("style", "text-decoration: line-through; color: #cccccc");
+                    } else {
+                        table_item.setAttribute("style", "text-decoration: ;")
+                    }
+                    table_item.innerHTML = ITEMS_ARRAY[j]["name"];
+                    table_row.appendChild(table_item);
+                } else if (i == 2) {
+                    let table_item = document.createElement("td");
+                    let delete_button = document.createElement("button");
+                    delete_button.setAttribute("id", j);
+                    delete_button.setAttribute("class", "btn btn-light text-secondary px-1 py-0 float-right");
+                    delete_button.innerHTML = "X";
+                    delete_button.addEventListener("click", checkDeleted);
 
-                table_item.appendChild(delete_button);
-                table_row.appendChild(table_item);
+                    table_item.appendChild(delete_button);
+                    table_row.appendChild(table_item);
+                }
             }
+            table_body.appendChild(table_row);
         }
-        table_body.appendChild(table_row);
     }
     table_main.appendChild(table_body);
     let row = document.getElementById("list_row");
@@ -84,8 +95,22 @@ function populateTable() {
 }
 
 function checkCompleted(e) {
-    if (document.getElementById(e.target.id).checked){
-        console.log("checked!");
+    if (document.getElementById(e.target.id).checked) {
+        ITEMS_ARRAY[parseInt(e.target.id)].completed = true;
+        LIST_ITEMS = ITEMS_ARRAY;
+        localStorage.setItem("todos", JSON.stringify(LIST_ITEMS));
 
+    } else {
+        ITEMS_ARRAY[parseInt(e.target.id)].completed = false;
+        LIST_ITEMS = ITEMS_ARRAY;
+        localStorage.setItem("todos", JSON.stringify(LIST_ITEMS));
     }
+    populateTable();
+}
+
+function checkDeleted(e) {
+    ITEMS_ARRAY[parseInt(e.target.id)].delete = true;
+    populateTable();
+    LIST_ITEMS = ITEMS_ARRAY;
+    localStorage.setItem("todos", JSON.stringify(LIST_ITEMS));
 }
--- js/page.js
+++ js/page.js
@@ -1,6 +1,7 @@
 /* Page Layout */
 let APP = document.getElementById("app");
-APP.setAttribute("class", "container");
+let CONTAINER = document.createElement("div");
+CONTAINER.setAttribute("class", "container");
 function init() {
     for (let i = 0; i < 4; i++) {
         if (i == 0) {
@@ -10,7 +11,8 @@ function init() {
             title_col.setAttribute("class", "col-12 display-4 text-center");
             title_col.innerHTML = "To-<span class='text-muted'>Dos</span>"
             title_row.appendChild(title_col);
-            APP.appendChild(title_row);
+            CONTAINER.appendChild(title_row);
+            APP.appendChild(CONTAINER);
         } else if (i == 1) {
             let input_row = document.createElement("div");
             let input_col = document.createElement("div");
@@ -21,7 +23,7 @@ function init() {
 
             input_row.setAttribute("class", "row");
             input_col.setAttribute("class", "col-12 mx-auto");
-            input_text.setAttribute("class", "w-100");
+            input_text.setAttribute("class", "w-100 form-control form-control-lg");
             input_text.setAttribute("style", "border: solid black 1px; border-radius: 5px;")
             input_text.setAttribute("type", "text");
             input_text.setAttribute("id", "input_text");
@@ -32,9 +34,13 @@ function init() {
 
             input_col.appendChild(input_text);
             input_row.appendChild(input_col);
-            APP.appendChild(input_row);
-            APP.appendChild(list_row);
+            CONTAINER.appendChild(input_row);
+            CONTAINER.appendChild(list_row);
+            APP.appendChild(CONTAINER);
         } else if (i == 2) {
+            let footer = document.createElement("footer");
+            footer.setAttribute("class","footer mt-auto mx-auto py-3");
+            let btn_contain = document.createElement("div");
             let btn_row = document.createElement("div");
             let btn_group = document.createElement("div");
             btn_group.setAttribute("class", "btn-group mx-auto");
@@ -56,7 +62,10 @@ function init() {
             }
             btn_row.setAttribute("class", "row");
             btn_row.appendChild(btn_group);
-            APP.appendChild(btn_row);
+            btn_contain.setAttribute("class", "container");
+            btn_contain.appendChild(btn_row)
+            footer.appendChild(btn_contain);
+            APP.appendChild(footer);
 
         }
         
